86718600cb117cd967480169a9b3582d8d4f2dbd
deepaksangwan09 <sangwandeepak008@gmail.com>
2025-10-09 11:04:47 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = " 33abc"
+//  let score = null
+ let score = undefined
+
+// console.log( typeof score )
+// console.log( typeof (score) ) 
+
+
+let valueInNumber = Number(score)
+// console.log( typeof valueInNumber)
+// console.log( valueInNumber )
+
+// " 33 " => 33
+// " 33abc " => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "hitesh"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log( booleanisLoggedIn);
+
+// 1=> true , 0 => false
+// "hitesh" => true , "" => false
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log( stringNumber)
+console.log( typeof stringNumber)
