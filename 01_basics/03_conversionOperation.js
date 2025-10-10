4cafc5bbd7b5151a9ba2adbc958ff7dd6622a580
deepaksangwan09 <sangwandeepak008@gmail.com>
2025-10-10 03:46:42 +0000

    conversion of datatypet

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,38 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log( stringNumber)
-console.log( typeof stringNumber)
+// console.log( stringNumber)
+// console.log( typeof stringNumber)
+
+
+// ***********************   OPERATIONS  ************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello "
+let str2 = "Deepak "
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log( "1" + 2)  // "1" + 2 =>  12
+// console.log( 1 + "2") // 1 + " 2 " => 12
+// console.log( 1 + 2 + "3") // 1 + 2 + " 3" => 33
+// console.log( "1" + 2 + 3) // "1" + 2 + 3 => 123
+
+// console.log( true) // true
+// console.log( +true) // 1
+// console.log( +false) // 0
+// console.log(+"") // 0
+
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+// console.log( num1 , num2 , num3)
+
+let gameCounter = 100
+gameCounter++  // postfix increment operator
+console.log(gameCounter)
+++gameCounter  // prefix increment operator
+console.log(gameCounter) 
