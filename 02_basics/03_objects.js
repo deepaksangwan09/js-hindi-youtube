324e0158928ef469e0b9085651c5fb3000b87794
deepaksangwan09 <sangwandeepak008@gmail.com>
2025-10-27 10:56:20 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,17 @@
 // singleton
+
+// Object.create
+
+// Object literals
+
+const JsUser = {
+    name : "Hitesh",
+    age : 18,
+    location : "Jaipur",
+    email : "hitesh@google.com",
+    isLoggedIn : false,
+    lastLoginDays : [ "Monday" , "Saturday" ]
+}
+
+console.log( JsUser )
+console.log(JsUser.email);
